ce2cbb89183807a6a7bf9076ce76f122f06978c6
gongyi01 <gongyi01@baidu.com>
2024-07-05 09:27:06 +0800

    update

--- assembly/index.ts
+++ assembly/index.ts
@@ -10,14 +10,24 @@ export function processImage(imageDataPtr: usize, width: i32, height: i32): void
     let imageData = changetype<Uint8Array>(imageDataPtr);
 
     let halfWidth: i32 = width / 2;
+    let factor: f32 = 0.9;
 
     for (let y: i32 = 0; y < height; y++) {
         for (let x: i32 = 0; x < halfWidth; x++) {
             let index: i32 = (y * width + x) * 4;
-            imageData[index] = 0;     // R
-            imageData[index + 1] = 0; // G
-            imageData[index + 2] = 0; // B
+            imageData[index] = clampToU8(imageData[index] * factor);     // R
+            imageData[index + 1] = clampToU8(imageData[index + 1] * factor); // G
+            imageData[index + 2] = clampToU8(imageData[index + 2] * factor); // B
         }
     }
 }
 
+function clampToU8(value: f32): u8 {
+  if (value < 0.0) {
+    return 0;
+  } else if (value > 255.0) {
+    return 255;
+  } else {
+    return <u8>value;
+  }
+}
